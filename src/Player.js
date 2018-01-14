888ac88c7738cb92972dc92624d13b1cf07bbaf6
MrJohnCoder <johnsmithcoder7@gmail.com>
2018-01-13 20:20:54 -0500

    Remove player.receivedEvents

--- src/Player.js
+++ src/Player.js
@@ -51,7 +51,6 @@ class Player extends EventEmitter {
         this.shard = shard;
         this.state = {};
         this.track = null;
-        this.receivedEvents = [];
         this.sendQueue = [];
         this.timestamp = Date.now();
     }
@@ -86,7 +85,6 @@ class Player extends EventEmitter {
      * @private
      */
     async sendEvent(data) {
-        this.receivedEvents.push(data);
         this.node.send(data);
         process.nextTick(() => this.checkEventQueue());
     }
